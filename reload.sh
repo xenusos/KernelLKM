d635b491498c8903af64ad7fd23a87d391914a22
Reece Wilson <reece@itsghost.me>
2018-07-21 21:00:24 +0100

    update ntos path

--- reload.sh
+++ reload.sh
@@ -1,5 +1,5 @@
 cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Kernel.xdll /Xenus/Kernel.xdll
-cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Mods/NTOS.xsys /Xenus/NTOS.xsys
+cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Modules/NTOS.XSYS /Xenus/SubSystems/NTOS.XSYS
 
 rmmod bootstrap2
 insmod bootstrap2.ko 
