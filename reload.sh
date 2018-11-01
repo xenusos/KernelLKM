50ffa0968511224088145ccee0fa5a6c0c31c08c
Reece W <me@reece.sx>
2018-11-01 19:14:53 +0000

    update dev copy script

--- reload.sh
+++ reload.sh
@@ -1,6 +1,8 @@
 cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Kernel.xdll /Xenus/Kernel/Kernel.xdll
 cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Modules/NTOS.XSYS /Xenus/Kernel/Plugins/NTOS.XSYS
 cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Modules/SelfTest.XSYS /Xenus/Kernel/Plugins/SelfTest.XSYS
+cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Modules/HDE.XSYS /Xenus/Kernel/Plugins/HDE.XSYS
+cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/Modules/DirectX.XSYS /Xenus/Kernel/Plugins/DirectX.XSYS
 cp /mnt/hgfs/Xenus/Kernel/Project/Binaries/LibOS.XSYS /Xenus/Kernel/LibOS.XSYS
 
 rmmod bootstrap2
